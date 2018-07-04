8ebc07bb6c1aa22c9d5caf21ea5890edff756b38
huynq <qhuydtvt@gmail.com>
2018-07-04 11:35:35 +0700

    Get ready for Animation videos

--- tests/test_box_collider.py
+++ tests/test_box_collider.py
@@ -4,4 +4,4 @@ b1 = BoxCollider(10, 10)
 b2 = BoxCollider(12, 48)
 b1.y = 100
 
-print(b1.overlap(b2))
+print(b1.overlap(b2))https://www.dropbox.com/s/y707tmuixan3xmr/C4T%20-%20Online%20-%20Collision%20-%202018-06-20-4.pptx?dl=0
